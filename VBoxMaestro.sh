f3f8079d73479792e36794b0dbdd77bc5f78786f
zwarag <harrys.kavan@gmail.com>
2014-06-14 00:11:26 +0200

    v1.0 finished added functions start save boot sleep   start     starts machines     takes X arguments     Example: ./VBoxMaestro start machine1 machine2
     save
        saves machines
        takes X arguments
        Example: ./VBoxMaestro save machine1 machine2
    
      boot
        Starts all machines that are saved in the ./boot directory.
        boot file config: Machines are separeted by line.
        Example: ./VBoxMaestro boot
    
      sleep
        Saves all machines that are currently running.
        This script uses 'VBoxManage list vms' to detect all running vms.
        Example: ./VBoxMaestro sleep
    
    Addidional Info:
    This script can only affect machines of the user who will execute this script.

---
+++ VBoxMaestro.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# VBoxManager bash script handler
+
+command=$1
+server="${@:2}"
+
+case "$command" in
+	'start')
+		echo "Will start machine: ""$server"
+		VBoxManage startvm $server --type=headless
+		echo "DONE; \n"
+	;;
+	'save')
+		echo "Will save machine: ""$server"
+
+		for server
+		do
+			if [ "$server" != "save" ] && [ "$server" != "start" ]
+			then
+				VBoxManage controlvm $server savestate
+			fi
+		done	
+		echo "DONE; \n"
+	;;
+	'boot')
+		echo "Will load from ./boot"
+		set -- $(<./boot)
+		VBoxManage startvm $@ --type=headless
+		echo "DONE; \n"
+	;;
+	'sleep')
+		echo "Will save all the machines!"
+		
+		VBoxManage list vms > ./.tmp
+		what=`grep -o '".*"' .tmp | sed 's/"//g'`
+		runningMachines=$what
+		
+		for runningMachines in `echo $runningMachines`
+		do
+			echo "Saving Maschine: $runningMachines"
+			VBoxManage controlvm $runningMachines savestate
+			echo "DONE; \n"
+		done
+
+	;;
+	'*')
+		echo "Unknown command: $command"
+	;;
+esac
